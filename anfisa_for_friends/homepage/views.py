03c11019da0ac6d817cd64ba2908e112f3121f19
Sergey Bogatyrev <bogatyrev.sergeyxxx@yandex.ru>
2025-05-22 14:10:16 +0500

    операторы

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,10 +1,17 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title', 'description').filter(is_on_main=True)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(
+            Q(is_on_main=True)
+            & Q(is_published=True)
+            | Q(title__contains='пломбир')
+            & Q(is_published=True))
     context = {
         'ice_cream_list': ice_cream_list,
     }
